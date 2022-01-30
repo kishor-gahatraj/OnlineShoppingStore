c920faf6d7f88ba8abd7c382cd990e5a638a5b46
kishor-gahatraj <90193890+kishor-gahatraj@users.noreply.github.com>
2022-01-30 20:55:11 +0545

    Add project files.

---
+++ dbo.Tbl_Product.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Tbl_Product] ON
+INSERT INTO [dbo].[Tbl_Product] ([ProductId], [ProductName], [CategoryId], [IsActive], [IsDelete], [CreatedDate], [ModifiedDate], [Description], [ProductImage], [IsFeatured], [Quantity], [Price]) VALUES (NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+SET IDENTITY_INSERT [dbo].[Tbl_Product] OFF
